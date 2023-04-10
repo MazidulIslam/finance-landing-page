72ff243e335175128bdc2e6181b3232360e44e91
MazidulIslam <mazidulislammym@gmail.com>
2023-04-10 22:25:30 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import Analytics from './components/Analytics';
+import Footer from './components/Footer';
 import Hero from './components/Hero';
 import Navbar from './components/Navbar';
+import Newsletter from './components/Newsletter';
+import Card from './components/Card';
 
 function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Analytics />
+      <Newsletter />
+      <Card />
+      <Footer />
     </div>
   );
 }
